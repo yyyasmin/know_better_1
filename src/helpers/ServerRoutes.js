1eb35fbff617aeb0117848074742efa1568b7ea8
unknown <you@example.com>
2024-05-04 21:24:36 +0300

    dummmy

--- src/helpers/ServerRoutes.js
+++ src/helpers/ServerRoutes.js
@@ -1,5 +1,5 @@
 export const PROXY_URL = "http://localhost:5000";
-export const RENDER_PROXY_URL = "https://knowbetter1-production.up.railway.app";  // mg-know-better-1 DEPLOY
+export const RENDER_PROXY_URL = "https://knowbetter1-production.up.railway.app";  // mg-know-better-1 Deploy
 
 export const CHOSEN_PROXY_URL = RENDER_PROXY_URL;
 //export const CHOSEN_PROXY_URL = PROXY_URL;
