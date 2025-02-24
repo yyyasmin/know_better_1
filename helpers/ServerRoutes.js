1fcf8fcd80aa32813db35bcaf58932cd0b0828c8
unknown <you@example.com>
2025-02-24 17:31:25 +0200

    server url adress

--- helpers/ServerRoutes.js
+++ helpers/ServerRoutes.js
@@ -1,8 +1,10 @@
 const PROXY_URL = "http://localhost:5000";
-const RENDER_PROXY_URL = "https://mg-young-kids-production.up.railway.app"; // mg-young_kids DEPLOY
+//const RENDER_PROXY_URL = "https://mg-young-kids-production.up.railway.app"; // mg-young_kids DEPLOY
+const RENDER_PROXY_URL = "https://knowbetter1-production.up.railway.app/";
 
 // Choose the appropriate proxy URL
-const CHOSEN_PROXY_URL = PROXY_URL; // Change to RENDER_PROXY_URL for production
+//const CHOSEN_PROXY_URL = PROXY_URL; // Change to RENDER_PROXY_URL for production
+const CHOSEN_PROXY_URL = RENDER_PROXY_URL; // Change to RENDER_PROXY_URL for production
 
 module.exports = {
   PROXY_URL,
